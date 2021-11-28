9039093789ee21257dd1346ce8e638d8eb0be43a
Tran Phuc Thoai <tranphucthoaiit@gmail.com>
2021-11-28 12:33:08 +0700

    update 28-11-12h30

--- generate-data.js
+++ generate-data.js
@@ -2,73 +2,99 @@ const faker = require('faker');
 const fs = require('fs');
 
 const randomAuthors = (n) => {
-    if (n <= 0) return [];
-    const authors = [];
+  if (n <= 0) return [];
+  const authors = [];
 
-    Array.from(new Array(n)).forEach(() => {
-        const author = {
-            id: faker.datatype.uuid(),
-            name: faker.name.findName(),
-            thumb: faker.image.avatar(),
-        };
-        authors.push(author);
-    });
-    return authors;
-}
+  Array.from(new Array(n)).forEach(() => {
+    const author = {
+      id: faker.datatype.uuid(),
+      name: faker.name.findName(),
+      thumb: faker.image.avatar(),
+    };
+    authors.push(author);
+  });
+  return authors;
+};
 
 const randomCategories = (n) => {
-    if (n <= 0) return [];
-    const categories = [];
+  if (n <= 0) return [];
+  const categories = [];
 
-    Array.from(new Array(n)).forEach(() => {
-        const category = {
-            id: faker.datatype.uuid(),
-            name: faker.commerce.department(),
-            backgroundColor: faker.vehicle.color(),
-        };
-        categories.push(category);
-    });
-    return categories;
-}
+  Array.from(new Array(n)).forEach(() => {
+    const category = {
+      id: faker.datatype.uuid(),
+      name: faker.commerce.department(),
+      backgroundColor: faker.vehicle.color(),
+    };
+    categories.push(category);
+  });
+  return categories;
+};
 
 const randomPosts = (categories, authors, n) => {
-    if (n <= 0) return [];
-    const posts = [];
+  if (n <= 0) return [];
+  const posts = [];
+  const thumbList = [
+    'https://f.hubspotusercontent40.net/hubfs/20002096/21-Jul-05-2021-03-03-31-83-AM.jpg',
+    'https://f.hubspotusercontent40.net/hubfs/20002096/45.jpg',
+    'https://f.hubspotusercontent40.net/hubfs/20002096/12-Jul-05-2021-03-06-05-81-AM.jpg',
+    'https://f.hubspotusercontent40.net/hubfs/20002096/18-Jul-05-2021-03-06-50-12-AM.jpg',
+    'https://f.hubspotusercontent40.net/hubfs/20002096/16-Jul-05-2021-03-08-31-68-AM.jpg',
+    'https://f.hubspotusercontent40.net/hubfs/20002096/23-Jul-05-2021-03-09-29-74-AM.jpg',
+    'https://f.hubspotusercontent40.net/hubfs/20002096/41-1.jpg',
+    'https://f.hubspotusercontent40.net/hubfs/20002096/51.jpg',
+    'https://f.hubspotusercontent40.net/hubfs/20002096/10-Jul-05-2021-03-12-21-03-AM.jpg',
+    'https://f.hubspotusercontent40.net/hubfs/20002096/35-1.jpg',
+    'https://f.hubspotusercontent40.net/hubfs/20002096/9-Jul-05-2021-03-14-55-24-AM.jpg',
+    'https://f.hubspotusercontent40.net/hubfs/20002096/42-1.jpg',
+    'https://f.hubspotusercontent40.net/hubfs/20002096/19-Jul-05-2021-03-22-08-59-AM.jpg',
+    'https://f.hubspotusercontent40.net/hubfs/20002096/1-Jul-05-2021-03-27-11-61-AM.jpg',
+    'https://f.hubspotusercontent40.net/hubfs/20002096/40.jpg',
+    'https://f.hubspotusercontent40.net/hubfs/20002096/48.jpg',
+    'https://f.hubspotusercontent40.net/hubfs/20002096/3-Jul-05-2021-03-24-28-25-AM.jpg',
+    'https://f.hubspotusercontent40.net/hubfs/20002096/30-3.jpg',
+    'https://f.hubspotusercontent40.net/hubfs/20002096/28-3.jpg',
+    'https://f.hubspotusercontent40.net/hubfs/20002096/11-Jul-05-2021-03-29-29-69-AM.jpg',
+    'https://f.hubspotusercontent40.net/hubfs/20002096/2.jpg',
+    'https://f.hubspotusercontent40.net/hubfs/20002096/33-2.jpg',
+  ];
 
-    for (const category of categories) {
-        for (const author of authors) {
-            Array.from(new Array(n)).forEach(() => {
-                const post = {
-                    authorId: author.id,
-                    categoryId: category.id,
-                    id: faker.datatype.uuid(),
-                    title: faker.lorem.sentence(),
-                    description: faker.lorem.paragraph(),
-                    createAt: faker.date.past(),
-                    updateAt: faker.date.past(),
-                    thumb: faker.image.imageUrl(1920, 1080),
-                    content: "",
-                };
-                posts.push(post);
-            });
-        }
+  for (const category of categories) {
+    for (const author of authors) {
+      for (const thumbTempalte of thumbList) {
+        Array.from(new Array(n)).forEach(() => {
+          const post = {
+            authorId: author.id,
+            categoryId: category.id,
+            id: faker.datatype.uuid(),
+            title: faker.lorem.sentence(),
+            description: faker.lorem.paragraph(),
+            createAt: faker.date.past(),
+            updateAt: faker.date.past(),
+            thumb: thumbTempalte,
+            content: '',
+          };
+          posts.push(post);
+        });
+      }
     }
-    return posts;
-}
+  }
+
+  return posts;
+};
 
 (() => {
+  const authors = randomAuthors(3);
+  const categories = randomCategories(3);
+  const posts = randomPosts(categories, authors, 1);
 
-    const authors = randomAuthors(3);
-    const categories = randomCategories(3);
-    const posts = randomPosts(categories, authors, 20);
+  const db = {
+    categories: categories,
+    posts: posts,
+    authors: authors,
+  };
 
-    const db = {
-        categories: categories,
-        posts: posts,
-        authors: authors
-    };
-
-    fs.writeFile('db.json', JSON.stringify(db), () => {
-        console.log("generate successfully");
-    });
-})()
+  fs.writeFile('db.json', JSON.stringify(db), () => {
+    console.log('generate successfully');
+  });
+})();
--- main.js
+++ main.js
@@ -1,49 +1,49 @@
-const jsonServer = require('json-server')
-const queryString = require('query-string')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require('json-server');
+const queryString = require('query-string');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
 
-server.use(middlewares)
+server.use(middlewares);
 
 server.get('/echo', (req, res) => {
-    res.jsonp(req.query)
-})
+  res.jsonp(req.query);
+});
 
-server.use(jsonServer.bodyParser)
+server.use(jsonServer.bodyParser);
 server.use((req, res, next) => {
-    if (req.method === 'POST') {
-        req.body.createdAt = Date.now()
-        req.body.updatedAt = Date.now()
-    } else if (req.method === 'PATCH') {
-        req.body.updatedAt = Date.now()
-    }
-    next()
-})
+  if (req.method === 'POST') {
+    req.body.createdAt = Date.now();
+    req.body.updatedAt = Date.now();
+  } else if (req.method === 'PATCH') {
+    req.body.updatedAt = Date.now();
+  }
+  next();
+});
 
 router.render = (req, res) => {
-    const headers = res.getHeaders();
-    const totalCountHeader = headers['x-total-count'];
+  const headers = res.getHeaders();
+  const totalCountHeader = headers['x-total-count'];
 
-    if (req.method === 'GET' && totalCountHeader) {
-        const queryParams = queryString.parse(req._parsedOriginalUrl.query);
+  if (req.method === 'GET' && totalCountHeader) {
+    const queryParams = queryString.parse(req._parsedOriginalUrl.query);
 
-        const result = {
-            data: res.locals.data,
-            pagination: {
-                _page: Number.parseInt(queryParams._page) || 1,
-                _limit: Number.parseInt(queryParams._limit) || 10,
-                _totalRows: Number.parseInt(totalCountHeader),
-            }
-        }
-        return res.jsonp(result);
-    }
-    res.jsonp(res.locals.data)
-}
+    const result = {
+      data: res.locals.data,
+      pagination: {
+        _page: Number.parseInt(queryParams._page) || 1,
+        _limit: Number.parseInt(queryParams._limit) || 10,
+        _totalRows: Number.parseInt(totalCountHeader),
+      },
+    };
+    return res.jsonp(result);
+  }
+  res.jsonp(res.locals.data);
+};
 
 // Use default router
 const PORT = process.env.PORT || 3000;
-server.use("/api", router)
+server.use('/api', router);
 server.listen(PORT, () => {
-    console.log('JSON Server is running')
-})
+  console.log('JSON Server is running');
+});
